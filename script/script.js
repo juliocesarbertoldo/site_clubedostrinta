40ba508f987a3c0c0f98d563c54646291609534f
JulioCesarBertoldo <112409550+JulioCesarBertoldo@users.noreply.github.com>
2023-02-02 18:36:47 -0300

    .

--- script/script.js
+++ script/script.js
@@ -4,7 +4,50 @@ botaoMenu.addEventListener('click', clickMenu)
 botaoPopUp.addEventListener('click', fecharPopUp)
 
 function fecharPopUp() {
-        promocao_template.style.display = 'none'
+    promocao_template.style.display = 'none'
+}
+
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        lista_menu.style.display = 'grid'
+        restante_item_cardapio_garrafa.style.display = 'block'
+        restante_item_cardapio_lata.style.display = 'block'
+        restante_item_cardapio_long.style.display = 'block'
+        restante_item_cardapio_refri.style.display = 'block'
+        restante_item_cardapio_espeto.style.display = 'block'
+        restante_item_cardapio_bebida.style.display = 'block'
+        restante_item_cardapio_salgado.style.display = 'block'
+        restante_item_cardapio_doce.style.display = 'block'
+    } else {
+        lista_menu.style.display = 'none'
+        restante_item_cardapio_garrafa.style.display = 'none'
+        restante_item_cardapio_lata.style.display = 'none'
+        restante_item_cardapio_long.style.display = 'none'
+        restante_item_cardapio_refri.style.display = 'none'
+        restante_item_cardapio_espeto.style.display = 'none'
+        restante_item_cardapio_bebida.style.display = 'none'
+        restante_item_cardapio_salgado.style.display = 'none'
+        restante_item_cardapio_doce.style.display = 'none'
+    }
+
+    if (window.innerWidth < 768) {
+        botaoItensGarrafa.innerHTML = 'Mostrar Tudo &#128070'
+        botaoItensGarrafa.style.background = '#c4ff8c'
+        botaoItensLata.innerHTML = 'Mostrar Tudo &#128070'
+        botaoItensLata.style.background = '#c4ff8c'
+        botaoItensLong.innerHTML = 'Mostrar Tudo &#128070'
+        botaoItensLong.style.background = '#c4ff8c'
+        botaoItensRefri.innerHTML = 'Mostrar Tudo &#128070'
+        botaoItensRefri.style.background = '#c4ff8c'
+        botaoItensEspeto.innerHTML = 'Mostrar Tudo &#128070'
+        botaoItensEspeto.style.background = '#c4ff8c'
+        botaoItensBebida.innerHTML = 'Mostrar Tudo &#128070'
+        botaoItensBebida.style.background = '#c4ff8c'
+        botaoItensSalgado.innerHTML = 'Mostrar Tudo &#128070'
+        botaoItensSalgado.style.background = '#c4ff8c'
+        botaoItensDoce.innerHTML = 'Mostrar Tudo &#128070'
+        botaoItensDoce.style.background = '#c4ff8c'
+    }
 }
 
 function clickMenu() {
